50f6e69ae07c39f23e40933d8e353acd22fa3d22
Sedki7 <sedkishabou7@gmail.com>
2023-12-22 18:12:34 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ForgiveMe'`, () => {
+  it(`should have as title 'Sorry'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ForgiveMe');
+    expect(app.title).toEqual('Sorry');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ForgiveMe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Sorry app is running!');
   });
 });
